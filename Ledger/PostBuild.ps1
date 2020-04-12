53db7c47d5d4950c6c68e773651a9bdc5fff10bb
Arthur <arthur@corenzan.com>
2020-04-12 01:14:15 -0300

    Fix build process and respective docs

--- Ledger/PostBuild.ps1
+++ Ledger/PostBuild.ps1
@@ -1,14 +1,14 @@
-param([String]$name = "")
-param([String]$dll = "")
-param([String]$install = "")
+param($name, $dll, $game, $config)
 
 $prefix = ".\dist\$name"
 
-Copy-Item .\Module -Destination $prefix -Recurse
-Copy-Item $dll -Destination $prefix\bin\Win64_Shipping_Client\
+Copy-Item ".\Module\*" -Destination "$prefix\" -Recurse -Force
+Copy-Item "$dll" -Destination "$prefix\bin\Win64_Shipping_Client" -Force
 
-(Get-Content -Path $prefix\SubModule.xml -Raw) -Replace '$(Version)', (Get-ChildItem $dll).VersionInfo.FileVersion | Set-Content -Path $prefix\SubModule.xml
+(Get-Content -Path "$prefix\SubModule.xml" -Raw) -Replace '\$\(Version\)', (Get-ChildItem "$dll").VersionInfo.FileVersion | Set-Content -Path "$prefix\SubModule.xml" -Force
 
-If ($install -ne "") {
-    Copy-Item $prefix -Destination $install\Modules\$name -Recurse
+Copy-Item "$prefix" -Destination "$game\Modules" -Recurse -Force
+
+If ($config -eq "Debug") {
+    Start-Process -FilePath "$game\bin\Win64_Shipping_Client\Bannerlord.exe" -ArgumentList "/singleplayer _MODULES_*Native*SandBoxCore*CustomBattle*SandBox*StoryMode*$name*_MODULES_"
 }
--- Ledger/README.md
+++ Ledger/README.md
@@ -8,10 +8,10 @@ Copy `env.example.xml` to `env.xml` and change the settings according to your en
 
 Run `dotnet build -p:Configuration=Debug|Release` to build the project.
 
-The `PostBuild.ps1` script will assemble the distribution directory with the module folder inside. On `Debug` configuration it'll automatically copy the module into the Modules directory within the game files and run the game.
+The `PostBuild.ps1` script will assemble the `dist` directory with the module folder inside. On `Debug` configuration it'll also install the module and run the game for a test-run.
 
 # Legal
 
-MIT ₢ 2020 Arthur Corenzan
+MIT © 2020 Arthur Corenzan
 
 This modification is not created by, affiliated with or sponsored by TaleWorlds Entertainment or its affiliates. The Mount & Blade II Bannerlord and related logos are intelectual property of TaleWorlds Entertainment. All rights reserved.
